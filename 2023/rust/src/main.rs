8b0e5c879d1939d6d2d4d479dde1c1a660bda768
nm2325 <nunoricardomendes@gmail.com>
2023-12-31 18:10:04 +0000

    rust: create command line tool

---
+++ 2023/rust/src/main.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+use log::{debug, info};
+use simplelog::{Config, LevelFilter, SimpleLogger};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(value_name = "day", index = 1)]
+    day: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = false)]
+    debug: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let log_level = if args.debug {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    };
+    let _logger = SimpleLogger::init(log_level, Config::default());
+
+    debug!("[DEBUG]");
+    info!("Running advent challenge {}!", args.day);
+}
