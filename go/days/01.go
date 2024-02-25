79e3c308472cf5cccb512e00c2b4b72f519bd1f7
nm2325 <nunoricardomendes@gmail.com>
2024-02-26 00:16:50 +0100

    go: make getResource work for any day

--- go/days/01.go
+++ go/days/01.go
@@ -53,7 +53,7 @@ func MatchDigit(input string, reverse bool) (string, error) {
 }
 
 func (d DayOne) Solve() (string, error) {
-	f, err := getResource("01")
+	f, err := getResource()
 	if err != nil {
 		return "", err
 	}
--- go/days/day-solver.go
+++ go/days/day-solver.go
@@ -1,20 +1,28 @@
 package advent
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 type DaySolver interface {
 	Solve() (string, error)
 }
 
-func getResource(day string) (*os.File, error) {
+func getResource() (*os.File, error) {
 	wd, err := os.Getwd()
 	if err != nil {
 		return nil, err
 	}
-	inputFile := filepath.Join(filepath.Dir(wd), "resources", day, "input.txt")
-
+	// Get name of file that calls this function
+	_, fpath, _, ok := runtime.Caller(1)
+	if !ok {
+		return nil, fmt.Errorf("cannot find filename of executable")
+	}
+	filename := filepath.Base(fpath)
+	inputFile := filepath.Join(filepath.Dir(wd), "resources", strings.Split(filename, ".")[0], "input.txt")
 	return os.Open(inputFile)
 }
